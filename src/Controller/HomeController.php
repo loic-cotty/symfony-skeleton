b6708c2a6e3840861c8d0a62fe993b8bcb41fdc6
Loïc Cotty <loic.cotty@gmail.com>
2024-11-01 12:54:00 +0100

    add and configure tailwindcss

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class IndexController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->render('home.html.twig');
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,9 @@ Encore
     // enables Sass/SCSS support
     //.enableSassLoader()
 
+    // enables PostCss support
+    .enablePostCssLoader()
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
